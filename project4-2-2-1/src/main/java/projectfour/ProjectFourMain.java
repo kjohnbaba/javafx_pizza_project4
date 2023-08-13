fa2a8d71dc3f06f0f1617faf2f75e6d0f825bc6a
kjohnbaba <105748315+kjohnbaba@users.noreply.github.com>
2023-08-13 10:46:39 -0400

    Update ProjectFourMain.java

--- project4-2-2-1/src/main/java/projectfour/ProjectFourMain.java
+++ project4-2-2-1/src/main/java/projectfour/ProjectFourMain.java
@@ -6,7 +6,7 @@ import javafx.application.Application;
  * The ProjectFourMain class is the driver class for the
  * project. It launches the application.
  *
- * @author Jaspreet Kaur, Kerimcan Baba
+ * @author Kerimcan Baba
  */
 public class ProjectFourMain {
 
