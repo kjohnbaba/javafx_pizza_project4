7bec210f4cc3fa2eb6af312909e740c7357575d7
kjohnbaba <105748315+kjohnbaba@users.noreply.github.com>
2023-08-14 09:29:04 -0400

    Update Crust.java

--- project4-2-2-1/src/main/java/projectfour/Crust.java
+++ project4-2-2-1/src/main/java/projectfour/Crust.java
@@ -4,7 +4,7 @@ package projectfour;
  * The Crust enum class is a class to store the crust
  * as constants.
  *
- * @author Jaspreet Kaur, Kerimcan Baba
+ * @author Kerimcan Baba
  */
 public enum Crust {
     DEEPDISH, PAN, STUFFED, BROOKLYN, THIN, HANDTOSSED
