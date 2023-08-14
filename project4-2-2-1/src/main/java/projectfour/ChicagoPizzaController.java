ebb5069a6f0f60c96aff068eb6e58a002bff433d
kjohnbaba <105748315+kjohnbaba@users.noreply.github.com>
2023-08-14 09:28:52 -0400

    Update ChicagoPizzaController.java

--- project4-2-2-1/src/main/java/projectfour/ChicagoPizzaController.java
+++ project4-2-2-1/src/main/java/projectfour/ChicagoPizzaController.java
@@ -15,7 +15,7 @@ import java.util.ResourceBundle;
  * pizza window. This class will create pizza orders of Chicago
  * style pizzas and add to the current order.
  *
- * @author Jaspreet Kaur, Kerimcan Baba
+ * @author Kerimcan Baba
  */
 public class ChicagoPizzaController implements Initializable{
 
