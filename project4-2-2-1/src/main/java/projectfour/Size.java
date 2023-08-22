e456852430793081e2e86ffdb553880e94aefe17
kjohnbaba <105748315+kjohnbaba@users.noreply.github.com>
2023-08-22 15:53:52 -0400

    Update Size.java

--- project4-2-2-1/src/main/java/projectfour/Size.java
+++ project4-2-2-1/src/main/java/projectfour/Size.java
@@ -4,7 +4,7 @@ package projectfour;
  * The Size enum class is a class to store the size
  * as constants.
  *
- * @author Jaspreet Kaur, Kerimcan Baba
+ * @author Kerimcan Baba
  */
 public enum Size {
     SMALL, MEDIUM, LARGE
