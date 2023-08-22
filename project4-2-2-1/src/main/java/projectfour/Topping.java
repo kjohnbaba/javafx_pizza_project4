e9bcb5f255d12216698f134c8f457279ef7fe8bf
kjohnbaba <105748315+kjohnbaba@users.noreply.github.com>
2023-08-22 15:53:08 -0400

    Update Topping.java

--- project4-2-2-1/src/main/java/projectfour/Topping.java
+++ project4-2-2-1/src/main/java/projectfour/Topping.java
@@ -4,7 +4,7 @@ package projectfour;
  * The Topping enum class is a class to store the toppings
  * as constants.
  *
- * @author Jaspreet Kaur, Kerimcan Baba
+ * @author Kerimcan Baba
  */
 public enum Topping {
     SAUSAGE, PEPPERONI, GREENPEPPER, ONION, MUSHROOM, BBQCHICKEN, PROVOLONE, CHEDDAR,
